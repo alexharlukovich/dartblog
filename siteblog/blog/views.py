a82ec17e638ca3cdf4dbc5f88726cd5401eddb73
Your Name <you@example.com>
2022-04-14 23:33:34 +0200

    lesson 12

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post, Category, Tag
+from django.db.models import F
 
 class Home(ListView):
     model = Post
@@ -27,12 +28,19 @@ class PostsByCategory(ListView):
         context['title'] = Category.objects.get(slug=self.kwargs['slug'])
         return context
 
+class PostsByTag(ListView):
+    pass
 
-def index(request):
-    return render(request, 'blog/index.html')
+class GetPost(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
 
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
 
-def get_post(request, slug):
-    return render(request, 'blog/category.html')
+
